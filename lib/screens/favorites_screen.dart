038e55b43c1490984ad1d25785c7156427e1b0ac
kabirsib349 <ibrahim.kabirsaleh@reseau.eseo.fr>
2026-01-05 16:35:26 +0100

    feat: ajout de l'écran favoris (structure de base)

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FavoritesScreen extends StatelessWidget{
+  const FavoritesScreen({super.key});
+
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Favoris"),
+    );
+  }
+}
