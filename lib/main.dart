faf69427c404f801341d9c6573ab697285abe4a6
kabirsib349 <ibrahim.kabirsaleh@reseau.eseo.fr>
2026-01-05 16:36:19 +0100

    feat: initialisation de l'application avec Hive pour le stockage local

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:angers_connect/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('favorites');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Angers Connecté',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainScreen()
+    );
+  }
+}
+
