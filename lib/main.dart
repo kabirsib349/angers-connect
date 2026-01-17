351fd033614bd2325c81723dbfa22f513e8d57e8
kabirsib349 <ibrahim.kabirsaleh@reseau.eseo.fr>
2026-01-17 14:31:29 +0100

    Integration des providers dans main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
-import 'package:angers_connect/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+
+import 'screens/main_screen.dart';
+import 'providers/work_provider.dart';
+import 'providers/favorites_provider.dart';
+import 'providers/location_provider.dart';
+import 'theme/theme.dart';
+import 'constants/app_constants.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  await Hive.openBox('favorites');
-  runApp(const MyApp());
+  await Hive.openBox(AppConstants.favoritesBoxName);
+  
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => WorkProvider()),
+        ChangeNotifierProvider(create: (_) => FavoritesProvider()..initHiveListener()),
+        ChangeNotifierProvider(create: (_) => LocationProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,11 +32,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Angers Connecté',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MainScreen()
+      title: AppConstants.appTitle,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const MainScreen(),
     );
   }
 }
